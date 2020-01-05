b082161f699fd2add510778d72f2cc185b1b4c35
KewenC <KewenC@outlook.com>
2020-01-06 00:45:42 +0800

    更新

--- app/src/main/java/com/kewenc/customview/MyView.kt
+++ app/src/main/java/com/kewenc/customview/MyView.kt
@@ -3,6 +3,7 @@ package com.kewenc.customview
 import android.content.Context
 import android.util.AttributeSet
 import android.util.Log
+import android.view.MotionEvent
 import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
 
@@ -17,5 +18,22 @@ class MyView : View {
 //        val mode = MeasureSpec.getMode(widthMeasureSpec)
 //        val size = MeasureSpec.getSize(widthMeasureSpec)
 //        Log.e("TAGF",mode.shr(30).toString() + "_" + size)
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+         when(event?.actionMasked) {
+             MotionEvent.ACTION_DOWN -> Log.e("TAGF","MotionEvent.ACTION_DOWN="+MotionEvent.ACTION_DOWN)
+             MotionEvent.ACTION_UP -> Log.e("TAGF","MotionEvent.ACTION_UP="+MotionEvent.ACTION_UP)
+             MotionEvent.ACTION_MOVE -> Log.e("TAGF","MotionEvent.ACTION_MOVE="+MotionEvent.ACTION_MOVE)
+             MotionEvent.ACTION_CANCEL -> Log.e("TAGF","MotionEvent.ACTION_CANCEL="+MotionEvent.ACTION_CANCEL)
+             MotionEvent.ACTION_OUTSIDE -> Log.e("TAGF","MotionEvent.ACTION_OUTSIDE="+MotionEvent.ACTION_OUTSIDE)
+             MotionEvent.ACTION_POINTER_DOWN -> Log.e("TAGF","MotionEvent.ACTION_POINTER_DOWN="+MotionEvent.ACTION_POINTER_DOWN)
+             MotionEvent.ACTION_POINTER_UP -> Log.e("TAGF","MotionEvent.ACTION_POINTER_UP="+MotionEvent.ACTION_POINTER_UP)
+         }
+//        when(event?.actionMasked) {
+//            MotionEvent.ACTION_POINTER_DOWN -> Log.e("TAGF","_MotionEvent.ACTION_POINTER_DOWN="+MotionEvent.ACTION_POINTER_DOWN)
+//            MotionEvent.ACTION_POINTER_UP -> Log.e("TAGF","_MotionEvent.ACTION_POINTER_UP="+MotionEvent.ACTION_POINTER_UP)
+//        }
+        return true
     }
 }
